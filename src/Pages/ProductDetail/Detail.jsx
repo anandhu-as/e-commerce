046f1f5ca3ea175ed67e39540a41b2fdc2a13e4d
anandhu-as <anandhuas010203@gmail.com>
2023-07-24 09:38:21 +0530

    updated

--- src/Pages/ProductDetail/Detail.jsx
+++ src/Pages/ProductDetail/Detail.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { about } from "../../NFT/Data";
-
 const Detail = ({ selectedProduct, setSelectedProduct }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10 bg-black bg-opacity-50 ">
@@ -10,12 +9,9 @@ const Detail = ({ selectedProduct, setSelectedProduct }) => {
           alt=""
           className="w-32 h-auto mx-auto mb-4"
         />
-
         <h2 className="text-white font-bold mb-4">{selectedProduct.name}</h2>
-
         <p className="text-white">$ {selectedProduct.price} USD</p>
         <p className="text-white"> {about}</p>
-
         <button
           className="px-4 py-2 mt-4 text-white bg-green-500 rounded-md hover:bg-blue-600"
           onClick={() => setSelectedProduct(null)}
