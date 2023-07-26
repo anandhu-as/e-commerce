5657c962435ef3b4707c9d95fbce4c340cd987da
anandhu-as <anandhuas010203@gmail.com>
2023-07-26 16:57:48 +0530

    fixed bugs on product card

--- src/Pages/Card/Card.jsx
+++ src/Pages/Card/Card.jsx
@@ -1,18 +1,27 @@
+
 import React from "react";
 import { useDispatch } from "react-redux";
+
 const Card = ({ name, price, image, setSelected }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className=" p-6 rounded-md shadow-md">
-        <h1 className="text-white">confirm product ? </h1>
-        <img src={image} alt={name} className="w-16 mt-2 h-16 mb-4" />
-        <h2 className="text-white font-bold mb-2">{name}</h2>
-        <h3 className="text-white mb-4">$ {price} USD</h3>
+      <div className="p-6 rounded-md shadow-md bg-gray-900 text-white">
+        <h2 className="text-white mb-2">Product Details - {name} ${price} USD</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          className="w-full px-4 py-2 mb-4 bg-gray-800 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 text-white"
+        />
+        <input
+          type="text"
+          placeholder="City | Pincode"
+          className="w-full px-4 py-2 mb-4 bg-gray-800 border rounded-lg focus:outline-none focus:ring focus:border-blue-300 text-white"
+        />
         <button
           className="px-4 py-2 bg-red-500 text-white rounded-lg mr-2"
           onClick={() => setSelected(null)}
         >
-          Close
+          Back
         </button>
         <button className="px-4 py-2 bg-green-400 text-white rounded-lg">
           Buy Now
