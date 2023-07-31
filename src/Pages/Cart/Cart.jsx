0852db37874d0a14145e9a8b1bfdc293c067a5eb
anandhu-as <anandhuas010203@gmail.com>
2023-07-31 08:54:08 +0530

    fixed the bug on total price

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { clear, remove } from "../../Redux/features/Product/ProductSlice";
+import {
+  clear,
+  incrementQuantity,
+  remove,
+} from "../../Redux/features/Product/ProductSlice";
 import { Link } from "react-router-dom";
 const Cart = () => {
   const dispatch = useDispatch();
@@ -12,7 +16,7 @@ const Cart = () => {
     cart.forEach((item) => {
       total = total + item.price * item.quantity;
     });
-    return total
+    return total;
   };
   return (
     <div className="flex pt-16">
@@ -43,8 +47,14 @@ const Cart = () => {
               <h2 className="text-white font-bold">{data.name}</h2>
               <h3 className="text-white">$ {data.price} USD</h3>
             </div>
+            <h2 className="text-white ml-16">{data.quantity}</h2>
             <div className="ml-auto">
-              <button className=" text-white mr-6">+</button>
+              <button
+                className=" text-white mr-6"
+                onClick={() => dispatch(incrementQuantity({ id: data.id }))}
+              >
+                +
+              </button>
               <button className=" text-white   mr-8">-</button>
               <button
                 className="px-4 py-2 text-white rounded-lg mr-6 bg-red-500"
--- src/Redux/features/Product/ProductSlice.js
+++ src/Redux/features/Product/ProductSlice.js
@@ -4,7 +4,6 @@ const initialState = {
   products: datas,
   cart: [],
   cartItemCount: 0,
-  total: 0,
   notifications: {
     sameProduct: [],
     cartNotification: [],
@@ -20,16 +19,15 @@ const ProductSlice = createSlice({
       const same = state.cart.find((cartItem) => cartItem.id === item.id);
       same
         ? state.notifications.sameProduct.push(item)
-        : state.cart.push({...item,quantity:1}) &&
+        : state.cart.push({ ...item, quantity: 1 }) &&
           state.notifications.cartNotification.push(item);
       state.cartItemCount = state.cart.length;
-    //  state.total = state.cartItemCount * price;///
+      //  state.total = state.cartItemCount * price;///
     },
     remove: (state, { payload }) => {
-      const { id, price, name } = payload;
+      const { id, name } = payload;
       state.cart = state.cart.filter((item) => item.id !== id);
       state.cartItemCount = state.cart.length;
-      state.total = state.cartItemCount * price;
       state.notifications.clearNotification.push(name);
     },
     clear: (state) => {
@@ -41,7 +39,19 @@ const ProductSlice = createSlice({
       state.notifications.sameProduct = [];
       state.notifications.clearNotification = [];
     },
+    incrementQuantity: (state, action) => {
+      const { id } = action.payload;
+      const sameID = state.cart.find((item) => item.id == id);
+      sameID ? (sameID.quantity += 1) : state.cart.push({ id, quantity: 1 });
+      state.cartItemCount = state.cart.length;
+    },
   },
 });
 export default ProductSlice.reducer;
-export const { add, remove, clear, clearSpam } = ProductSlice.actions;
+export const {
+  add,
+  remove,
+  clear,
+  clearSpam,
+  incrementQuantity,
+} = ProductSlice.actions;
